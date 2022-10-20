39498e033c8b4936b0a537d6a8052815002ac86e
Chris Maki <c.william.developer@gmail.com>
2022-10-20 13:34:36 -0500

    Fixed routes

--- server/routes/cart.router.js
+++ server/routes/cart.router.js
@@ -1,17 +1,19 @@
 const express = require('express');
-const routes= express.Router();
+const router= express.Router();
 const pool = require('../modules/pool');
 
 //GET students (Chris work)
 
-routes.get('/', (req,res) => {
+router.get('/', (req,res) => {
     const sqlText = `SELECT * FROM shopping_cart`;
     pool.query(sqlText)
     .then((result) => {
-        res.send(result.row);
+        res.send(result.rows);
     })
     .catch((err) => {
-        console.log("Error making database query ${sqlText}",err)
+        console.log(`Error making database query ${sqlText}`,err)
         res.sendStatus(500);
     });
 });
+
+module.exports = router;
