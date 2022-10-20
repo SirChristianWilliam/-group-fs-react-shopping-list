fb45fffe3821b8c1e2f850961b2d4209d6972956
Mohamed Yusuf <Mohamed-yusuf2@hotmail.com>
2022-10-20 14:37:47 -0500

    add new function- addCart

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,19 +1,44 @@
 import React from 'react';
+import axios from 'axios';
 import CartForm from '../CartForm/CartForm.jsx';
 import CartItem from '../CartItem/CartItem.jsx';
 import CartList from '../CartList/CartList.jsx';
 import Header from '../Header/Header.jsx'
+
 import './App.css';
 
 
 function App() {
+
+
+
+    function addCart(newCartItems){
+        console.log('In addCart')
+
+        axios({
+            method: 'POST',
+            url: '/cart',
+            data: newCartItems
+         })
+        .then(response => {
+            console.log('Response is:',response)
+
+                getCart();
+
+        })
+        .catch(err => {
+          alert('Error Adding Cart');
+          console.log(err);
+        })
+    }
     return (
         <div className="App">
             <Header />
             <main>
                 <p>Under Construction...</p>
             </main>
-            <CartForm />
+            <CartForm 
+            addCart={addCart}/>
             <CartList />
             <CartItem />
         </div>
