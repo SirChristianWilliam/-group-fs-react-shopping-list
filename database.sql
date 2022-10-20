9f10c9b0bb36dee503ced778574a0b0ed5c92846
Chris Maki <c.william.developer@gmail.com>
2022-10-20 11:32:34 -0500

    Link

--- database.sql
+++ database.sql
@@ -1,2 +1,13 @@
 -- Don't forget to add your create table SQL 
 -- It is also helpful to include some test data
+
+
+CREATE TABLE shopping_cart (
+    id SERIAL PRIMARY KEY,
+    item varchar(80),
+    quantity INTEGER,
+    unit varchar(9),
+    purchased BOOLEAN DEFAULT FALSE
+);
+INSERT INTO shopping_cart ("item, quantity, purchased")
+VALUES ("Banana", 60, false), ("More Banana", 3, false), ("Juan", 1, false);
