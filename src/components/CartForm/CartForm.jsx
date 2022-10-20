4fea5fc59a48bf715bfb024f2255d58ef0addf70
Mohamed Yusuf <Mohamed-yusuf2@hotmail.com>
2022-10-20 13:38:17 -0500

    save progress- add handle Submit function

--- src/components/CartForm/CartForm.jsx
+++ src/components/CartForm/CartForm.jsx
@@ -1,6 +1,16 @@
 import React, { useState } from 'react';
-function CartForm(){
+function CartForm({getCart}){
+    
+    const [cart, setCart] = useState('')
 
+    const handleSubmit = (evt) =>{
+        evt.preventDefault();
+        getCart(cart)
+        clearCart()
+    }
+    const clearCart = () =>{
+        
+    }
 
     return(
         <h2>CartForm</h2>
