90db2f383fc65551e3f0031abae3f1634a9a5932
Chris Maki <c.william.developer@gmail.com>
2022-10-21 11:16:02 -0500

    styled table

--- src/components/CartItem/CartItem.jsx
+++ src/components/CartItem/CartItem.jsx
@@ -1,22 +1,23 @@
 function CartItem({cartitem, buyItem, removeItem}) {
 
-const onBuy = () => {
-    buyItem(cartitem);
-}
-
-const onRemove = () => {
-    removeItem(cartitem);
-}
-
-    return (
-        <>
-        <li>
-            {cartitem.item} {cartitem.quantity} {cartitem.unit}
-            <button className="buyBtn" onClick={onBuy}>Buy</button>
-            <button className="removeBtn" onClick={onRemove}>Remove</button>
-        </li>
-        </>
-    );
-}
-
-export default CartItem;
+    const onBuy = () => {
+        buyItem(cartitem);
+    }
+    
+    const onRemove = () => {
+        removeItem(cartitem);
+    }
+    
+        return (
+        
+                <tr>
+                    <td>{cartitem.item}</td> <td>{cartitem.quantity}</td> <td>{cartitem.unit}</td> <td>False</td>
+                    <td><button className="buyBtn" onClick={onBuy}>Buy</button></td>
+                    <td><button className="removeBtn" onClick={onRemove}>Remove</button></td>
+                </tr>
+        
+        );
+    }
+    
+    export default CartItem;
+    
